1b06ef2954ce55e67661c596b364f27b3d79b7f0
Shivansh Rajolia <shivansh.rajolia28@gmail.com>
2019-02-22 23:27:20 +0530

    webpack node preset Fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
 
 const config = {
+    target: 'node',
+    externals: [nodeExternals()],
     entry: './app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        libraryTarget: 'commonjs2'
     },
     module: {
         rules: [
             {
-                use: 'babel-loader',
                 test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                          ['@babel/preset-env', {
+                            'targets': {
+                              'node': 'current'
+                            }
+                          }]
+                        ]
+                      }
+                },
                 exclude: /node_modules/
             }
         ]
